3475df8564f1d6c63568e8a54033931593fc0148
Artem Pavlov <rtmpvlv@gmail.com>
2021-12-13 17:59:54 +0300

    Completed lesson 2 task

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import {PLACES_COUNT_TYPES} from '../types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import PageNotFound from '../404/404';
+import Favorites from '../favorites/favorites';
+import Login from '../login/login';
 import Main from '../main/main';
+import Property from '../property/property.jsx';
+import {PLACES_COUNT_TYPES} from '../types';
+import {AppRoute} from '../../constants';
 
 const App = (props) => {
   const {placesCount} = props;
-
   return (
-    <Main
-      placesCount = {placesCount}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.MAIN}>
+          <Main
+            placesCount = {placesCount}
+          />
+        </Route>
+        <Route exact path={AppRoute.LOGIN}>
+          <Login />
+        </Route>
+        <Route exact path={AppRoute.FAVORITES}>
+          <Favorites />
+        </Route>
+        <Route exact path={AppRoute.ROOM}>
+          <Property />
+        </Route>
+        <Route>
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
